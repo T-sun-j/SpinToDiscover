175d1dafd48adbd2526813fd1a805b20f399a907
T-sun-j <15836320807@163.com>
2025-11-21 17:24:42 +0800

    feat: 添加新的页面路由和图标组件
    - 在app-paths-manifest.json中添加新的页面路由
    - 在lucide-react中新增eye、eye-off、loader-circle、square-pen和star图标组件
    - 更新webpack运行时配置和热更新文件

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("0e7a03d2d2465c91")
+/******/ 		__webpack_require__.h = () => ("ebc77e7edcf503bc")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
@@ -163,6 +163,11 @@
 /******/ 			var r = fn();
 /******/ 			return r === undefined ? result : r;
 /******/ 		}
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	(() => {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/require chunk loading */
